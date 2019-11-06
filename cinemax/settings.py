3792e221294d8866c96a3bec66fb5daf7485b9b8
Stephen <xmanstephen@gmail.com>
2019-11-07 04:37:43 +0530

    Sample mockup created

--- cinemax/settings.py
+++ cinemax/settings.py
@@ -51,10 +51,13 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'cinemax.urls'
 
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+TEMP_DIR = os.path.join(BASE_DIR, 'templates')
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMP_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,3 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
+STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
---
+++ cinemaxpr/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class CinemaxprConfig(AppConfig):
+    name = 'cinemaxpr'
