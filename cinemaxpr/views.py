f03c77e8ad36078e1939770ce09e36dd6b48ec39
Stephen <xmanstephen@gmail.com>
2019-11-07 05:33:12 +0530

    Purchase requisition portal page created

--- cinemaxpr/views.py
+++ cinemaxpr/views.py
@@ -2,5 +2,13 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    print('HIIIIIIIIIIIIIIIIII')
     return render(request, 'index.htm')
+
+def dashboard(request):
+    return render(request, 'dashboard.htm', {'view': 'dashboard'})
+
+def memo(request):
+    return render(request, 'memo.htm', {'view': 'memo'})
+
+def budget(request):
+    return render(request, 'budget.htm', {'view': 'budget'})
